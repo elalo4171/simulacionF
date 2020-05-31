a2c9ca12e36d1a03995b376dd9c87cec9118001b
Eduardo Garcia <elalo417@gmail.com>
2020-05-31 13:00:55 -0500

    Page de corridas creada

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:simulacion/src/pages/generador_adictivo.dart';
 import 'package:simulacion/src/pages/generadpr_multiplicativo.dart';
 import 'package:simulacion/src/pages/home_page.dart';
 import 'package:provider/provider.dart';
+import 'package:simulacion/src/pages/prueba_corridas.dart';
 import 'package:simulacion/src/pages/show_numeros.dart';
 import 'package:simulacion/src/providers/generador_provider.dart';
 import 'package:simulacion/src/theme/tema.dart';
@@ -25,6 +26,7 @@ class MyApp extends StatelessWidget {
           'ver':(BuildContext context)=>ShowNumeros(),
           'adictivo':(BuildContext context)=>Adictivo(),
           'multiplicativo':(BuildContext context)=>Multiplicativo(),
+          'corridas':(BuildContext context)=>CorridasArribaAbajo(),
         },
         initialRoute: 'multiplicativo',
         theme: miTema,
---
+++ lib/src/pages/prueba_corridas.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CorridasArribaAbajo extends StatefulWidget {
+  CorridasArribaAbajo({Key key}) : super(key: key);
+
+  @override
+  _CorridasArribaAbajoState createState() => _CorridasArribaAbajoState();
+}
+
+class _CorridasArribaAbajoState extends State<CorridasArribaAbajo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       body: Container(),
+    );
+  }
+}
